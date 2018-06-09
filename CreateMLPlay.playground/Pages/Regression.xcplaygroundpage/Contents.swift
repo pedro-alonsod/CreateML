6f7ab85fd6c8788af80f008e621ba172bff1c5e9
PedroA <pedro.alonsod@gmail.com>
2018-06-08 20:45:09 -0500

    CreateML Start

--- CreateMLPlay.playground/Pages/Regression.xcplaygroundpage/Contents.swift
+++ CreateMLPlay.playground/Pages/Regression.xcplaygroundpage/Contents.swift
@@ -1,10 +1,14 @@
 import Foundation
 import CreateML
-
+//Specify data
 let houseData = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/pedroalonso/Documents/Apps/CreateML/RegressionML/HouseData.csv"))
 let (trainingCSVData, testCSVData) = houseData.randomSplit(by: 0.8, seed: 0)
-//2
+//2 create model
 let pricer = try MLRegressor(trainingData: houseData, targetColumn: "MEDV")
-//3
+//Evaluation
+
+let metrics = pricer.evaluation(on: testCSVData)
+print(testCSVData.description)
+//Save model
 let csvMetadata = MLModelMetadata(author: "Pedro Alonso", shortDescription: "A model used to determine the price of a house based on some features.", version: "1.0")
 try pricer.write(to: URL(fileURLWithPath: "/Users/pedroalonso/Documents/Apps/CreateML/RegressionML/HouseData.mlmodel"), metadata: csvMetadata)
--- CreateMLPlay.playground/Pages/TextClassification.xcplaygroundpage/Contents.swift
+++ CreateMLPlay.playground/Pages/TextClassification.xcplaygroundpage/Contents.swift
@@ -1,15 +1,20 @@
 import CreateML
 import Foundation
 //1
+//Specify data
 let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/pedroalonso/Documents/Apps/CreateML/TextClassifier/spam.json"))
 let (trainingData, testingData) = data.randomSplit(by: 0.8, seed: 5)
+//Create model
 let spamClassifier = try MLTextClassifier(trainingData: trainingData, textColumn: "text", labelColumn: "label")
 //2
+//Print metrics
 let trainingAccuracy = (1.0 - spamClassifier.trainingMetrics.classificationError) * 100
 let validationAccuracy = (1.0 - spamClassifier.validationMetrics.classificationError) * 100
 //3
+//Evaluate model
 let evaluationMetrics = spamClassifier.evaluation(on: testingData)
 let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
 //4
+//Save model
 let metadata = MLModelMetadata(author: "Pedro Alonso", shortDescription: "A model trained to classify spam messages", version: "1.0")
 try spamClassifier.write(to: URL(fileURLWithPath: "/Users/pedroalonso/Documents/Apps/CreateML/TextClassifier/SpamDetector.mlmodel"), metadata: metadata)

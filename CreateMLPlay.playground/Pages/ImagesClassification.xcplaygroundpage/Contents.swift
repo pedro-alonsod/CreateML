416a922d789a9896a5e59c9081daedf7bedca272
PedroA <pedro.alonsod@gmail.com>
2018-06-29 17:50:18 -0500

    Test in Cocoa
    See how much iOS knowledge I can transfer

--- CreateMLPlay.playground/Pages/ImagesClassification.xcplaygroundpage/Contents.swift
+++ CreateMLPlay.playground/Pages/ImagesClassification.xcplaygroundpage/Contents.swift
@@ -3,7 +3,11 @@ import PlaygroundSupport
 import Cocoa
 
 let view = NSView(frame: NSRect(x: 0, y: 0, width: 200, height: 400))
-
+let lbTest = NSButton(frame: NSRect(x: 20, y: 20, width: 50, height: 20))
+lbTest.title = "Cocoa"
+lbTest.addToolTip(NSRect(x: 0, y: 0, width: 20, height: 20), owner: lbTest, userData: nil)
+lbTest.action = Selector("Hello")
+//view.addSubview(lbTest)
 PlaygroundPage.current.liveView = view
 //PlaygroundPage.current.needsIndefiniteExecution = true
 
